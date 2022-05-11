31c82e3299950fe42ffe058fe4f603631385da6d
KerollosEmd <91436830+KerollosEmd@users.noreply.github.com>
2022-05-11 10:05:41 +0200

    Fix Logic and sequance of main code

--- Code/AROM_main.c
+++ Code/AROM_main.c
@@ -11,7 +11,7 @@
  */
 #include "STD_Types.h"
 #include "BIT_MATH.h"
-#define F_CPU 		1000000UL
+#define F_CPU 		8000000UL
 #include <util/delay.h>
 
 /*
@@ -20,7 +20,6 @@
  */
 #include "DIO_interface.h"
 #include "ADC_interface.h"
-
 
 /*
  * @Layer: HAL
@@ -31,7 +30,6 @@
 /*
  * End of Importing Section
  */
-
 
 /*
  * @defines
@@ -44,29 +42,25 @@
 /*
  * @Global_Variables
  */
-u16 AROM_Au16Resistances[]={
-		220,				//R1
-		560,                //R2
-		1000,               //R3
-		2200,               //R4
-		4700,               //R5
-		10000,              //R6
-		22000               //R7
-};
+u16 AROM_Au16Resistances[] = { 208,				//R1
+		548,                //R2
+		983,                //R3
+		2110,               //R4
+		4550,               //R5
+		9730,               //R6
+		23600               //R7
+		};
 
-u8 AROM_Au8Channels[]={
-		0x01,  		 //CH1
+u8 AROM_Au8Channels[] = { 0x01,  		 //CH1
 		0x02,        //CH2
 		0x04,        //CH3
 		0x08,        //CH4
 		0x10,        //CH5
 		0x20,        //CH6
 		0x40         //CH7
-};
+		};
 
-u16 AROM_u16DigitalReadings[7]={
-		0
-};
+u16 AROM_Au16DigitalReadings[7] = { 0 };
 /*
  * End of Global Variables
  */
@@ -79,71 +73,109 @@ u16 AROM_u16DigitalReadings[7]={
  * End of Functions Prototypes
  */
 
-
 /*
  * @main function
  */
 
-int main(void){
+int main(void) {
 
 	/*
 	 * @Local_Variables
 	 */
-	u8  Local_u8Index=0;
-	u16 Local_u16DigitalValue=0;
-	f32 Local_f32ResX=0.0;
-	f32 Local_f32VoltX_mv=0;
+	u8 Local_u8Index = 0;
+	u16 Local_u16DigitalValue = 0;
+	f32 Local_f32ResX = 0;
+	f32 Local_f32VoltX_mv = 0;
+	f32 Local_f32Loading = 0;
+
 	/*
 	 * End of Local Variables
 	 */
 
 	/*
-	 * @Initialization Functions of Peripherals
+	 * @Local Strings
+	 */
+	u8 Local_u8String_Load[] = "====Loading====";
+	u8 Local_u8String_R1[] = "R1=";
+	u8 Local_u8String_Rx[] = "Rx=";
+	u8 Local_u8String_ADC[] = "ADC=";
+	u8 Local_u8String_ConvEr[] = "Conversion Error";
+	u8 Local_u8String_mv[] = "mV";
+	/*
+	 * End of Local Strings
+	 */
+
+	/*
+	 * @Initialization Functions of Peripherals & Devices
 	 */
 	DIO_voidInit();
 	LCD_voidInit();
 	ADC_voidInit();
+	ADC_u8SetPrescaler(ADC_u8_PS_CLK_BY_2);
 	/*
 	 * End of Initializations
 	 */
-	u8 Local_u8String[]="RES=";
-	LCD_voidWriteString(Local_u8String);
+
 	/*
 	 * @Super_Loop
 	 */
-	while(1){
-
+	while (1) {
+		LCD_voidWriteCommand(LCD_Clear);
+		LCD_voidWriteString(Local_u8String_Load);
 		/*Swapping between the 7 Channels and Take Readings*/
-		for (Local_u8Index=0;Local_u8Index<7;Local_u8Index++){
-			DIO_u8SetPortValue(DIO_u8_PORTC,AROM_Au8Channels[Local_u8Index]); //Setting High one channel and the other to Low
-			
+		for (Local_u8Index = 0; Local_u8Index < 7; Local_u8Index++) {
+			DIO_u8SetPortValue(DIO_u8_PORTC, AROM_Au8Channels[Local_u8Index]); //Setting High one channel and the other to Low
+
 			/*
-			 * if condition to check that there is no error return from the ADC_u8GetDigitalValueSync 
+			 * The if condition to check that there is no error return from the ADC_u8GetDigitalValueSync
 			 * hint: >>>> the instruction lines beside the debug comment, it for debugging and will be deleted 
 			 *            at the Release state.
 			 */
-			if(STD_TYPES_OK==ADC_u8GetDigitalValueSync(ADC_u8_SINGLE_ADC7, &Local_u16DigitalValue)){
+			if (STD_TYPES_OK
+					== ADC_u8GetDigitalValueSync(ADC_u8_SINGLE_ADC7,
+							&Local_u16DigitalValue)) {
 				/*
 				 * Taking the reading and assign it to the Array of Readings to the corresponding reading element 
 				 */
-				AROM_u16DigitalReadings[Local_u8Index]=Local_u16DigitalValue;
-				
-				LCD_voidGoToXY(0,LCD_u8_LINE_2);				//debug
-				LCD_voidWritNumber(Local_u16DigitalValue);      //debug
-				_delay_ms(500);									//debug
+				AROM_Au16DigitalReadings[Local_u8Index] = Local_u16DigitalValue;
 
-			}else{
+				Local_f32Loading = 14.3 * (Local_u8Index + 1);
+				LCD_voidGoToXY(6, LCD_u8_LINE_2);
+				LCD_voidWritNumber((u32) Local_f32Loading);
+				LCD_voidWriteChar('%');
+				_delay_ms(10);
+
+			} else {
 				/*
 				 * if the ADC_u8GetDigitalValueSync Function return Error
 				 * in this case it assign 0 to the corresponding Digital Reading in the array
 				 */
-				AROM_u16DigitalReadings[Local_u8Index]=0;
+				AROM_Au16DigitalReadings[Local_u8Index] = 0;
+				LCD_voidWriteCommand(LCD_Clear);
+				LCD_voidWriteString(Local_u8String_ConvEr);
 			}
 		}
 		/*End of for loop*/
+		Local_f32Loading = 0;
+
+		/*
+		 * Start debugging section to see the content of the reading array
+		 */
+//		for (Local_u8Index = 0; Local_u8Index < 7; Local_u8Index++) {
+//			LCD_voidWriteCommand(LCD_Clear);
+//			LCD_voidWriteString(Local_u8String_R1);
+//			LCD_voidWritNumber(AROM_Au16Resistances[Local_u8Index]);
+//			LCD_voidGoToXY(0, LCD_u8_LINE_2);
+//			LCD_voidWriteString(Local_u8String_ADC);
+//			LCD_voidWritNumber(AROM_Au16DigitalReadings[Local_u8Index]);
+//			_delay_ms(500);
+//		}
+		/*
+		 * End of debugging section
+		 */
 
 		/*Swapping for checking for valid ranges and Calculate the equivalent Resistance*/
-		for(Local_u8Index=6;Local_u8Index>=0;Local_u8Index--){
+		for (Local_u8Index = 6; Local_u8Index >= 0; Local_u8Index--) {
 			/*@description:
 			 * It checks the array from bottom to top of it as if the range of Digital reading
 			 * (corresponding to large resistors) is nearly equals to (1/2) of ADC Range (1024)
@@ -151,13 +183,30 @@ int main(void){
 			 * and it belongs to its range. So there is no need to check the other readings as
 			 * they will be very large and inaccurate.
 			 */
-			if(AROM_u16DigitalReadings[Local_u8Index]>=440){
-				Local_f32VoltX_mv=(f32)(Local_u16DigitalValue*(5000UL))/1024.0;
-				Local_f32ResX=Local_f32VoltX_mv*AROM_Au16Resistances[Local_u8Index]/(4300UL-Local_f32VoltX_mv);
-				LCD_voidGoToXY(4,LCD_u8_LINE_1);
-				LCD_voidWritNumber((u32)Local_f32ResX);
-				LCD_voidWriteChar(0xF4);
-				LCD_voidWriteString("   ");
+			if ((AROM_Au16DigitalReadings[Local_u8Index] >= 430)
+					|| (AROM_Au16DigitalReadings[Local_u8Index] < 430
+							&& Local_u8Index == 0)) {
+				Local_f32VoltX_mv =
+						(f32) (AROM_Au16DigitalReadings[Local_u8Index]
+								* (5000UL)) / 1024;
+				Local_f32ResX = (Local_f32VoltX_mv
+						* AROM_Au16Resistances[Local_u8Index])
+						/ (4440UL - Local_f32VoltX_mv);
+				LCD_voidWriteCommand(LCD_Clear);
+				LCD_voidWriteString(Local_u8String_Rx);
+				LCD_voidWritNumber((u32) Local_f32ResX);
+				LCD_voidWriteChar(0xF4);					// The OHM symbol
+				LCD_voidWriteChar(' ');
+
+				LCD_voidWritNumber(Local_f32VoltX_mv);                   //debug
+				LCD_voidWriteString(Local_u8String_mv);					//debug
+				LCD_voidGoToXY(0, LCD_u8_LINE_2);						 //debug
+				LCD_voidWriteString(Local_u8String_ADC);                 //debug
+				LCD_voidWritNumber(AROM_Au16DigitalReadings[Local_u8Index]); //debug
+				LCD_voidWriteChar(' ');                                  //debug
+				LCD_voidWriteString(Local_u8String_R1);					//debug
+				LCD_voidWritNumber(AROM_Au16Resistances[Local_u8Index]); //debug
+				_delay_ms(3000);
 				break;
 			}
 		}
